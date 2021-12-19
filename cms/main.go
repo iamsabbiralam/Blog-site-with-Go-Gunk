b2a2e6538f7da664332bf16b3fb3ff908f34e3de
Sabbir Alam <sabbiralam833@gmail.com>
2021-12-19 23:30:46 +0600

    create cms part and insert data in database

---
+++ cms/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"gunkBlog/cms/handler"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/schema"
+	"github.com/gorilla/sessions"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+
+	tcb "gunkBlog/gunk/v1/category"
+)
+
+func main() {
+	config := viper.NewWithOptions(
+		viper.EnvKeyReplacer(
+			strings.NewReplacer(".", "_"),
+		),
+	)
+	config.SetConfigFile("cms/env/config")
+	config.SetConfigType("ini")
+	config.AutomaticEnv()
+	if err := config.ReadInConfig(); err != nil {
+		log.Printf("error loading configuration: %v", err)
+	}
+
+	decoder := schema.NewDecoder()
+	decoder.IgnoreUnknownKeys(true)
+	
+	store := sessions.NewCookieStore([]byte(config.GetString("session.key")))
+
+	conn, err := grpc.Dial(
+		fmt.Sprintf("%s:%s", config.GetString("categoryService.host"), config.GetString("categoryService.port")),
+	grpc.WithInsecure(),
+)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tc := tcb.NewCategoryServiceClient(conn)
+	r := handler.New(decoder, store, tc)
+
+	host, port := config.GetString("server.host"), config.GetString("server.port")
+
+	log.Printf("Server starting on http://%s:%s", host, port)
+	if err:= http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), r); err != nil {
+		log.Fatal(err)
+	}
+
+}
