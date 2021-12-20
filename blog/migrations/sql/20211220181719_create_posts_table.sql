1aa48deef8fdd39e9c9567707eb1d349461fe53f
Sabbir Alam <sabbiralam833@gmail.com>
2021-12-20 18:33:13 +0600

    create posts table with foriegn key into database

--- blog/migrations/sql/20211220181719_create_posts_table.sql
+++ blog/migrations/sql/20211220181719_create_posts_table.sql
@@ -4,9 +4,14 @@ CREATE TABLE IF NOT EXISTS posts(
     id SERIAL NOT NULL,
     cat_id INTEGER NOT NULL,
     title TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL,
     image TEXT NOT NULL,
 
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    CONSTRAINT fk_category
+    FOREIGN KEY(cat_id) 
+    REFERENCES categories(id)
+    ON DELETE SET NULL
 );
 -- +goose StatementEnd
 
